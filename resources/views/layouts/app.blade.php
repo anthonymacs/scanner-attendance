91662836a673ad999f734a8878f636f20107abd8
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-02-13 12:49:24 +0700

    cleaned

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ config('app.name', 'Scanner') }}</title>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
     @livewireStyles
 </head>
 <body>
